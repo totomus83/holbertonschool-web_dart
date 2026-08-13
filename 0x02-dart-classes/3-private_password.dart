8184f389e1356c2c89cff2dc1a73146a07cba347
totomus83 <tom.dibellonio@gmail.com>
2026-08-13 14:13:56 +0200

    Task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,22 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    bool hasUpper = _password.contains(RegExp(r'[A-Z]'));
+    bool hasLower = _password.contains(RegExp(r'[a-z]'));
+    bool hasDigit = _password.contains(RegExp(r'[0-9]'));
+
+    return hasUpper && hasLower && hasDigit;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
